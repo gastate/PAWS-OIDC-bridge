08dbd7bb482c059525ef428216f10391b682648c
Polyergic <me@shadsterling.com>
2017-06-22 10:43:02 -0400

    Fix environments

--- PAWS-OIDC-bridge-env.dev.js
+++ PAWS-OIDC-bridge-env.dev.js
@@ -2,10 +2,10 @@ PAWS_OIDC_bridge.ENV = {
 	loginSettings: {
 		authority: "https://idp-d.gsu.edu/",
 		client_id: "@!DE0A.9C27.805E.9101!0001!2E8F.F37E!0008!48AF.805B",
-		redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/redirect",
-		silent_redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/silent",
-		popup_redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/popup",
-		post_logout_redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/logout",
+		redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/redirect.html",
+		silent_redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/silent.html",
+		popup_redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/popup.html",
+		post_logout_redirect_uri: "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/logout.html",
 		response_type: 'id_token token',
 		scope: "openid profile gsupersonpantherid",
 		automaticSilentRenew: true,
@@ -15,4 +15,9 @@ PAWS_OIDC_bridge.ENV = {
 		revokeAccessTokenOnSignout: true
 	}
 }
+PAWS_OIDC_bridge.uri_logout = "https://cdn-dev.gsu.edu/PAWS-OIDC-bridge/logout_helper.html";
 PAWS_OIDC_bridge.status_elm = "PAWS-OIDC-bridge-status";
+PAWS_OIDC_bridge.iframe_style = { 
+	width: "100%",
+	height: "20em",
+}
--- PAWS-OIDC-bridge-env.js
+++ PAWS-OIDC-bridge-env.js
@@ -2,10 +2,10 @@ PAWS_OIDC_bridge.ENV = {
 	loginSettings: {
 		authority: "https://idp-d.gsu.edu/",
 		client_id: "@!DE0A.9C27.805E.9101!0001!2E8F.F37E!0008!48AF.805B",
-		redirect_uri: "http://localhost:8080/redirect",
-		silent_redirect_uri: "http://localhost:8080/silent",
-		popup_redirect_uri: "http://localhost:8080/popup",
-		post_logout_redirect_uri: "http://localhost:8080/logout",
+		redirect_uri: "http://localhost:8080/redirect.html",
+		silent_redirect_uri: "http://localhost:8080/silent.html",
+		popup_redirect_uri: "http://localhost:8080/popup.html",
+		post_logout_redirect_uri: "http://localhost:8080/logout.html",
 		response_type: 'id_token token',
 		scope: "openid profile gsupersonpantherid",
 		automaticSilentRenew: true,
@@ -15,9 +15,9 @@ PAWS_OIDC_bridge.ENV = {
 		revokeAccessTokenOnSignout: true
 	}
 }
-PAWS_OIDC_bridge.uri_logout = "http://localhost:8080/logout_helper";
+PAWS_OIDC_bridge.uri_logout = "http://localhost:8080/logout_helper.html";
 PAWS_OIDC_bridge.status_elm = "PAWS-OIDC-bridge-status";
 PAWS_OIDC_bridge.iframe_style = { 
 	width: "100%",
-	height: "15em",
+	height: "20em",
 }
